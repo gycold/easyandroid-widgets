ac2efd386bd961869529bee1532d8406472e3d94
= <=>
2022-07-11 15:47:28 +0800

    update layout

---
+++ easy-widgets/src/main/java/com/easyandroid/widgets/layout/SquareRelativeLayout.java
@@ -0,0 +1,39 @@
+package com.easyandroid.widgets.layout;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * package: com.easyandroid.widgets.layout.SquareRelativeLayout
+ * author: gyc
+ * description:
+ * time: create at 2022/7/11 15:46
+ */
+public class SquareRelativeLayout extends RelativeLayout {
+
+    public SquareRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public SquareRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public SquareRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Set a square layout.
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
